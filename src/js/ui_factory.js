c1955893a835b7d682662ab045ec8acbef6c4c16
Johnson Zhou <johnson.zhou@simplyuseful.io>
2023-04-30 19:11:25 +1000

    feat: completed table factory

--- src/js/ui_factory.js
+++ src/js/ui_factory.js
@@ -17,6 +17,7 @@
     @param {Array} tableData.data - Array of table data consisting of objects.
     @param {Object} tableData.data[] - An object holding table data that has
       keys consistent with the headers field.
+    @return {HTMLElement} A HTML element that can be added to the DOM.
    */
   window.UiFactory.createTable = (tableData) => {
     console.log(tableData)
@@ -26,15 +27,13 @@
     const table = document.createElement("table")
     table.classList.add("table")
 
-    // Create table artifacts
+    // Create table artefacts
     const tr = document.createElement("tr")
     const th = document.createElement("th")
     const td = document.createElement("td")
-    const tbody = document.createElement("tbody")
-
-    const thead = document.createElement("thead")
 
     // Build the header
+    const thead = document.createElement("thead")
     const tableHeadRow = tr.cloneNode()
     headers.map((label) => {
       const cell = th.cloneNode()
@@ -44,10 +43,29 @@
     })
     thead.appendChild(tableHeadRow)
 
-    //TODO Build the table body
+    // Build the table body
+    const tbody = document.createElement("tbody")
+    data.map((row) => {
+      const tableRow = tr.cloneNode()
+
+      // Check that row data matches headers in number of elements
+      if (Object.keys(row).length != headers.length) {
+        console.error("Table Factory: Row data does not match headers")
+        return
+      }
+
+      Object.values(row).map((content) => {
+        const cell = td.cloneNode()
+        cell.innerHTML = content
+        tableRow.appendChild(cell)
+      })
+
+      tbody.appendChild(tableRow)
+    })
 
     // Build the table
     table.appendChild(thead)
+    table.appendChild(tbody)
 
     return table
   }
