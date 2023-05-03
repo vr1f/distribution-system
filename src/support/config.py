fba5770dcaa7a9ced89622dfe6865ab41108aeef
Yi-Cheng Peng <alexpeng0401@gmail.com>
2023-05-03 11:41:09 +1000

    include hash secret

--- src/support/config.py
+++ src/support/config.py
@@ -47,9 +47,9 @@ class AWSConfig(Config):
         self.DB_PORT="5432"
         self.DB_DATABASE="vr1_db1"
         self.DB_PASSWORD=input("Enter DB password:")
-        # self.SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-        # self.ALGORITHM = "HS256"
-        # self.ACCESS_TOKEN_EXPIRE_MINUTES = 30
+        self.SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+        self.ALGORITHM = "HS256"
+        self.ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
         
 
