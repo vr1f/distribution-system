c6adc99ee4557b7bf1bfeefbec329fd24694a22e
jkengs <107338480+jkengs@users.noreply.github.com>
2023-05-04 16:22:06 +1000

    feat: add log out function

---
+++ src/js/base.js
@@ -0,0 +1,25 @@
+/**
+  Methods relating to the base html
+ */
+(() => {
+  const overwriteCookie = () => {
+    const currTime = new Date().getTime();
+    const currUTC = new Date(currTime).toUTCString();
+    document.cookie =`token=; expires=${currUTC}`;
+  }
+
+  const bindButtonActions = () => {
+    document.getElementById("log-out").addEventListener("click", logout);
+  }
+
+  const logout = () => {
+    overwriteCookie();
+  }
+  /**
+  Run on load
+  */
+  window.onload = () => {
+    bindButtonActions();
+  }
+
+})() 
