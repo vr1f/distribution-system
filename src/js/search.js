6ce9c65b63914d36a908594d9caafeb552c35bc9
jkengs <107338480+jkengs@users.noreply.github.com>
2023-05-09 18:48:27 +1000

    feat: add fetch function

--- src/js/search.js
+++ src/js/search.js
@@ -5,13 +5,43 @@
  */
 (() => {
 
+
+  // Check if first name is given
+  const isValid = (firstName, type) => {
+    return (firstName != "" && type != 0);
+  }
+
   const search = async () => {
     const firstName = document.getElementById("firstname").value;
     const lastName = document.getElementById("lastname").value;
     // Recipient => Value = 1  Donor => Value = 2
     const type = document.getElementById("type").value;
-    // TO DO: POST REQUEST
-    // TO DO: VALID FORM CHECK
+    if (isValid(firstName, type)) {
+      // If all fields are entered
+      const result = await fetch("/search", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          firstname: firstName,
+          lastname: lastName,
+          type: type,
+        }),
+      })
+      .then((response) => {
+        if (response.status != 200) {
+          throw new Error("Bad Request");
+        }
+        return response.json();
+      })
+      .then((json) => {
+        // TO DO: Unpack and create table with UiFactory
+      })
+      .catch((error) => {
+        alert(error.message);
+      })
+    } else {
+      alert("Please fill the first name and type!")
+    }
   }
 
   const bindButtonActions = () => {
@@ -33,7 +63,7 @@
 
   /**
      Run on load
- */
+  */
   window.addEventListener("load", () => {
     bindButtonActions();
   })
