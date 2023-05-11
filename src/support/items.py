50eaea02e49fb9df19a55d292971c901412afbd8
Declan Dempsey <ddempsey@student.unimelb.edu.au>
2023-05-11 12:29:40 +1000

    feat: items data class

---
+++ src/support/items.py
@@ -0,0 +1,39 @@
+# =========================================================================
+#
+# VR1FAMILY CHARITY DISTRIBUTION IT SYSTEM
+#
+# Data structures for items, categories and kits
+#
+# =========================================================================
+
+from pydantic import BaseModel
+import enum
+
+class StatusEnum(enum.Enum):
+    LOW = "low"
+    MEDIUM = "medium"
+    HIGH = "high"
+
+class Item(BaseModel):
+    """
+    A base item data unit representing any item
+
+    Args:
+
+
+    """
+    item_id: int = None
+    item_name: str
+    item_quantity: int
+
+class Category(BaseModel):
+    """
+    A category data unit that defines a category
+
+    Args:
+
+    
+    """
+    category_id: int
+    category_name: str
+    status: StatusEnum
