b307e2f1235e4befb986ec5fff34b1c1771a26bc
Johnson Zhou <johnson.zhou@simplyuseful.io>
2023-05-22 14:49:29 +1000

    fix: Remove gender from items

--- src/db/db_builder.py
+++ src/db/db_builder.py
@@ -176,35 +176,9 @@ class Item_DB(Base):
     ingredients = Column(String)
     allergen_info = Column(String)
     size = Column(Enum(Size))
-    gender = Column(Enum(Gender))
     category_id = Column(Integer, ForeignKey("category.category_id", ondelete="CASCADE", onupdate="CASCADE"))
     __mapper_args__ = {'inherit_condition': category_id == Categories.category_id}
 
-# =======================
-# FOOD_ITEM
-# Inherits Item class. Further includes food details
-# PK is item_id from item table
-# =======================
-""" class Food_Item(Item_DB):
-    __tablename__ = 'food_item'
-    item_id = Column(Integer, ForeignKey("item.item_id", ondelete="CASCADE", onupdate="CASCADE"), primary_key=True)
-    expiry_date = Column(Date)
-    ingredients = Column(String)
-    allergen_info = Column(String)
-    __mapper_args__ = {'inherit_condition': item_id == Item_DB.item_id}
- """
-# =======================
-# CLOTHING_ITEM
-# Inherits Item class. Further clothing details
-# PK is item_id from item table
-# =======================
-""" class Clothing_Item(Item_DB):
-    __tablename__ = 'clothing_item'
-    item_id = Column(Integer, ForeignKey("item.item_id", ondelete="CASCADE", onupdate="CASCADE"), primary_key=True)
-    size = Column(Enum(Size))
-    gender = Column(Enum(Gender))
-    __mapper_args__ = {'inherit_condition': item_id == Item_DB.item_id}
- """
 # =======================
 # AID_KIT
 # Aid kits that include a combination of items
